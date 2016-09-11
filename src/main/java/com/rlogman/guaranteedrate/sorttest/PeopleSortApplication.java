1a5a13e339c95e624d968d4e98af68082002ad7d
Rodrigo Lopez-Guzman <rodrigo.lopez@globant.com>
2016-09-11 18:07:24 -0500

    Initial Application context main declaration (Spring boot)

---
+++ src/main/java/com/rlogman/guaranteedrate/sorttest/PeopleSortApplication.java
@@ -0,0 +1,12 @@
+package com.rlogman.guaranteedrate.sorttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleSortApplication.class, args);
+	}
+}
